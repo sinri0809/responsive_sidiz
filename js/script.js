03e5d5a7c30b8bb8df737cba8e279de8215407a7
sinri0809 <dev.1lliodqb@gmail.com>
2021-09-12 23:22:06 +0900

    sidiz_midnightwithvodkatonic_ver.1

---
+++ js/script.js
@@ -0,0 +1,81 @@
+'use strict';
+window.onload = function () {
+  $('.--close').click(function () { 
+    $('.modal').stop().fadeOut();
+  });
+  $('.--confirm').click(function () { 
+    $('.modal').stop().fadeOut();
+  });
+
+  var swp_v = new Swiper('.swp-v', {
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true,
+    },
+    autoplay: {
+      delay: 3500,
+      pauseOnMouseEnter: true,
+    },
+    speed: 1500,
+    navigation: {
+      nextEl: '.controll-next',
+      prevEl: '.controll-prev',
+    }
+  });
+  $('.swp-v').mouseenter(function () {
+    swp_v.autoplay.stop();
+  });
+  $('.swp-v').mouseleave(function () {
+    swp_v.autoplay.start();
+  });
+
+  
+  var depth2 = $('.nav__depth2');
+  var depth2_timer;
+
+  function depth2Out() {
+    depth2.stop().fadeOut();
+  }
+  
+  var menu_about = $('#about-s');
+  var dpeth2_about = $('.--about-s');
+  var menu_product = $('#product');
+  var dpeth2_product = $('.--product');
+  var menu_support = $('#support');
+  var dpeth2_support = $('.--support');
+  
+
+
+  menu_about.mouseenter(function () {
+    dpeth2_about.stop().fadeIn();
+  });
+  menu_about.mouseleave(function () {
+    depth2_timer = setTimeout(depth2Out, 100);
+    console.log(depth2_timer);
+  });
+
+  menu_product.mouseenter(function () {
+    dpeth2_product.stop().fadeIn();
+  });
+  menu_product.mouseleave(function () {
+    depth2_timer = setTimeout(depth2Out, 100);
+    console.log(depth2_timer);
+  });
+
+  menu_support.mouseenter(function () {
+    dpeth2_support.stop().fadeIn();
+  });
+  menu_support.mouseleave(function () {
+    depth2_timer = setTimeout(depth2Out, 100);
+    console.log(depth2_timer);
+  });
+
+
+  depth2.mouseenter(function () {
+    clearTimeout(depth2_timer);
+  });
+  depth2.mouseleave(function () {
+    depth2Out();
+  });
+
+}
