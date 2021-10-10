4a386d6f06d2ea0dbe8d3fa0016cd2049e0e7487
sinri0809 <dev.1lliodqb@gmail.com>
2021-10-10 21:02:51 +0900

    sidiz_javascript_ver.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 window.onload = function () {
   // modal
   $('.--close').click(function () { 
@@ -9,7 +8,8 @@ window.onload = function () {
     $('.modal').stop().fadeOut();
   });
 
-  var swp_v = new Swiper('.swp-v', {
+  // swiper
+  let swp_v = new Swiper('.swp-v', {
     effect: 'fade',
     fadeEffect: {
       crossFade: true,
@@ -24,8 +24,6 @@ window.onload = function () {
       prevEl: '.controll-prev',
     }
   });
-
-  // swiper
   $('.swp-v').mouseenter(function () {
     swp_v.autoplay.stop();
   });
@@ -33,51 +31,77 @@ window.onload = function () {
     swp_v.autoplay.start();
   });
 
-  
-  var depth2 = $('.nav__depth2');
-  var depth2_timer;
+  // depth2 event jQuery
 
-  function depth2Out() {
-    depth2.stop().fadeOut();
+  // let depth2_timer;
+
+  // let menu_lst = $('.nav__depth1').find('> li');
+  // let menu_depth2 = $('.nav__depth2');
+  
+  // function depth2Out() {
+  //   menu_depth2.stop().fadeOut();
+  // }
+  // for (let index in menu_lst){
+  //   menu_lst.eq(index).mouseenter(function(){
+  //     menu_depth2.eq(index).stop().fadeIn();
+  //   });
+  //   menu_lst.eq(index).mouseleave(function(){
+  //     depth2_timer = setTimeout(depth2Out, 20);
+  //   })
+  // }
+  // menu_depth2.mouseenter(function () {
+  //   clearTimeout(depth2_timer);
+  // });
+  // menu_depth2.mouseleave(function () {
+  //   clearTimeout(depth2_timer);
+  //   depth2Out();
+  // });
+
+  // depth2 show and hide
+  let menu_lst = document.querySelectorAll('.nav__depth1 > li'); 
+  let depth2_lst = document.querySelectorAll('.nav__depth2');
+  let depth2_timer;
+  let depth2_out_timer;
+  
+  function depth2Out(obj){
+    // clearTimeout(depth2_timer);
+    obj.classList.add('--disappear');
+    // waiting when --disappear animation end
+    depth2_out_timer = setTimeout(()=>{
+      obj.style.display = 'none';
+      console.log(obj);
+    }, 600);
+  }
+  function depth2In(obj){
+    obj.classList.remove('--disappear');
+    obj.style.display = 'block';
+    console.log(obj);
   }
   
-  var menu_about = $('#about-s');
-  var dpeth2_about = $('.--about-s');
-  var menu_product = $('#product');
-  var dpeth2_product = $('.--product');
-  var menu_support = $('#support');
-  var dpeth2_support = $('.--support');
-  
-  menu_about.mouseenter(function () {
-    dpeth2_about.stop().fadeIn();
-  });
-  menu_about.mouseleave(function () {
-    depth2_timer = setTimeout(depth2Out, 50);
-    console.log("about"+depth2_timer);
-  });
-
-  menu_product.mouseenter(function () {
-    dpeth2_product.stop().fadeIn();
-  });
-  menu_product.mouseleave(function () {
-    depth2_timer = setTimeout(depth2Out, 50);
-    console.log("product"+depth2_timer);
-  });
-
-  menu_support.mouseenter(function () {
-    dpeth2_support.stop().fadeIn();
-  });
-  menu_support.mouseleave(function () {
-    depth2_timer = setTimeout(depth2Out, 50);
-    console.log("support"+depth2_timer);
+  menu_lst.forEach((item, index)=>{
+    item.addEventListener('mouseenter', (e) =>{
+      e.stopPropagation();
+      depth2In(depth2_lst[index]);
+      clearTimeout(depth2_out_timer);
+      // depth2_timer = setTimeout(()=>{
+      //   depth2Out(depth2_lst[index]);
+      // }, 2000);3
+      console.log('enter')
+    });
+    item.addEventListener('mouseleave', (e) =>{
+      e.stopPropagation();
+      // just start disappear
+      depth2Out(depth2_lst[index]);
+      console.log('leave')
+      // but when depth2 enter,
+      depth2_lst[index].addEventListener('mouseenter', ()=>{
+        clearTimeout(depth2_out_timer);
+        // clearTimeout(depth2_timer);
+        depth2In(depth2_lst[index]);
+        console.log('depth2');
+      });
+    });
   });
 
-  depth2.mouseenter(function () {
-    clearTimeout(depth2_timer);
-  });
-  depth2.mouseleave(function () {
-    clearTimeout(depth2_timer);
-    depth2Out();
-  });
 
 }
